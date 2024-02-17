cf5537dbe57b75da4652f5f12e7326102a6d976a
Faner201 <62674880+Faner201@users.noreply.github.com>
2024-02-17 14:25:18 +1000

    Event addition to an option type

--- options.go
+++ options.go
@@ -5,6 +5,26 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
+// LoggableEvent defines the events a log line can be added on.
+type LoggableEvent uint
+
+const (
+	// StartCall is a loggable event representing start of the gRPC call.
+	StartCall LoggableEvent = iota
+	// FinishCall is a loggable event representing finish of the gRPC call.
+	FinishCall
+	// PayloadReceived is a loggable event representing received request (server) or response (client).
+	// Log line for this event also includes (potentially big) proto.Message of that payload in
+	// "grpc.request.content" (server) or "grpc.response.content" (client) field.
+	// NOTE: This can get quite verbose, especially for streaming calls, use with caution (e.g. debug only purposes).
+	PayloadReceived
+	// PayloadSent is a loggable event representing sent response (server) or request (client).
+	// Log line for this event also includes (potentially big) proto.Message of that payload in
+	// "grpc.response.content" (server) or "grpc.request.content" (client) field.
+	// NOTE: This can get quite verbose, especially for streaming calls, use with caution (e.g. debug only purposes).
+	PayloadSent
+)
+
 var (
 	// DefaultCodeToLevelFunc is the default implementation code to level logic.
 	// returns Info on codes.OK and Error in all other cases
@@ -22,8 +42,9 @@ var (
 	}
 
 	defaultOptions = &options{
-		levelFunc: DefaultCodeToLevelFunc,
-		shouldLog: DefaultDeciderFunc,
+		levelFunc:      DefaultCodeToLevelFunc,
+		shouldLog:      DefaultDeciderFunc,
+		loggableEvents: []LoggableEvent{StartCall, FinishCall},
 	}
 )
 
@@ -50,9 +71,17 @@ func WithDecider(f Decider) Option {
 	}
 }
 
+// WithLogOnEvents customizes on what events the gRPC interceptor should log on.
+func WithLogOnEvents(events ...LoggableEvent) Option {
+	return func(o *options) {
+		o.loggableEvents = events
+	}
+}
+
 type options struct {
-	levelFunc CodeToLevel
-	shouldLog Decider
+	levelFunc      CodeToLevel
+	shouldLog      Decider
+	loggableEvents []LoggableEvent
 }
 
 func evaluateOptions(opts []Option) *options {
